5a0043c710c79f025c3e2a33e8046657b42685e1
Shoujian YUN <jiangya68@gmail.com>
2023-10-02 22:19:35 +0800

    ccodes_10_02     循环分支

---
+++ code_10_2/code_10_2/test.c
@@ -0,0 +1,53 @@
+ #define  _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//void test()
+//{
+//	static int a = 0;
+//	a++;
+//	printf("%d ", a);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//	return 0;
+//}
+//
+
+//int  main()
+//{
+//	int  a = 10; 
+//	&a;
+//	printf("%p" , &a);
+//	return 0;
+////}
+//
+//int main()
+//{
+//	printf("%zu", sizeof(char*));
+//	printf("%zu", sizeof(int*));
+//	printf("%zu", sizeof(long long*)); //8
+//	return 0;
+////}
+//
+//
+//int main() {
+//
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch) ;
+//		printf("%c",ch);
+//	}
+//	return 0;
+//
+//}
+
---
+++ code_10_2/code_10_2/test1.c
@@ -0,0 +1,37 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+/*int main()
+{
+	*///printf( "%04d", 1);//0001   
+	//printf( "%.4d", 1);//0001  
+	//printf( "%.4f", 123.12345);//0001  
+
+
+	/*int num = printf("abc");
+	printf("%d" , num);*/
+
+	//while (1)
+	//{
+	//	printf("%d " , rand());
+	//	Sleep(1000);
+	//}
+
+//	int main()
+//	{
+//		char input[20] = { 0 };
+//		system("shutdown -s -t 120");
+//again:
+//		printf("请注意，你的电脑在60秒内关机，如果输入：我是猪，就取消关机\n");
+//		scanf("%s",input);
+//		if (strcmp(input, "我是猪") == 0) 
+//		{
+//			system("shutdown -a");
+//		}
+//		else 
+//		{
+//			goto again;
+//		}
+//
+//	return 0;
+//	}
